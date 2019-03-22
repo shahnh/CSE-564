7c1def2d02b12a3c28d9e0cdc0524eff48dbea2e
shahnh <shahnh>
2019-03-22 12:18:36 -0400

    Notes are added.

---
+++ Try/src/Tester.java
@@ -0,0 +1,18 @@
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		/*
+		 * int m = 1000;
+		 * double a = (Math.pow(5, .5)-1)/2;
+		 * double ans = m * ((65 * a) % 1);
+		 * System.out.println((int)ans);
+		 */
+		
+		double a = Math.pow(2, .5) -1;
+		System.err.println((int)(4*3*a));
+		
+		
+	}
+
+}
